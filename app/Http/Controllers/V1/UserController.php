396a632cee3bd54ccd590bac51ade49d3631187d
rupandangol <rupandangol87@gmail.com>
2024-01-07 07:32:02 +0000

    User Login And Register

---
+++ app/Http/Controllers/V1/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\RegisterUserRequest;
+use App\Http\Requests\V1\UserLoginRequest;
+use App\Http\Resources\V1\UserCollection;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function login(UserLoginRequest $request) 
+    {   
+        $user=User::where(['email'=>$request->email])->first();
+        if(!$user || !Hash::check($request->password,$user->password)){
+            return response()->json([
+                'status'=>'failed',
+                'message'=>'Invalid Credentials'
+            ],401);
+        }
+        $data['token']=$user->createToken($request->email)->plainTextToken;
+        $data['user']=$user;
+        $response=[
+            'status'=>'success',
+            'message'=>'User Logged in Successfully',
+            'data'=>$data
+        ];
+       return response()->json($response,200);
+    }
+    public function register(RegisterUserRequest $request) 
+    {
+        $user=User::create($request->all());
+        $data['token']=$user->createToken($request->email)->plainTextToken;
+        $data['user']=$user;
+        $response = [
+            'status' => 'success',
+            'message' => 'User is created successfully.',
+            'data' => $data,
+        ];
+        return response()->json($response,201);
+    }
+
+    public function logout(Request $request) {
+        
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status'=>'success',
+            'message'=>'Logout Successfully'
+        ],200);
+    }
+
+    public function index()  {
+        return new UserCollection(User::all());
+    }
+
+    public function show(string $id)  {
+        // return User::findOrFail($id);
+        return new UserResource(User::findOrFail($id));
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\V1\CategoryController;
 use App\Http\Controllers\V1\ProductController;
+use App\Http\Controllers\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +22,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group(['prefix'=>'V1'],function(){
-    // Route::get('/categories',[CategoryController::class,'index']);
+    Route::post('/users/register',[UserController::class,'register'])->name('api-user-register');
+    Route::post('/users/login',[UserController::class,'login'])->name('api-user-login');
+
+    
+    Route::get('/users',[UserController::class,'index'])->name('api-user-index');
+    Route::get('/users',[UserController::class,'index'])->name('api-user-index');
+    Route::get('/users/{id}',[UserController::class,'show'])->name('api-user-show');
+    Route::group(['middleware'=>'auth:sanctum'],function(){
+    Route::post('/users/logout',[UserController::class,'logout'])->name('api-user-logout');
     Route::resource('/categories',CategoryController::class);
-    Route::resource('/products',ProductController::class);
+        Route::resource('/products',ProductController::class);
+    });
 });
